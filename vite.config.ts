0b3ba3cfd30b6c5f55967200161b239e64fd5542
barathsuresh <barathsuresh3@gmail.com>
2026-05-19 11:59:32 -0700

    rollback

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/portfolio/', // Change to '/' if deploying to username.github.io
 })
