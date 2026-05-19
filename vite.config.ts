5b988b5d0219aec41e70b19277df54f3c59e3d19
barathsuresh <barathsuresh3@gmail.com>
2026-05-19 11:54:02 -0700

    some changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // Change to '/' if deploying to username.github.io
+  base: '/',
 })
